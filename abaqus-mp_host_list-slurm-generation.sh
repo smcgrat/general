452bbf9265291f1099ab41378e95d9aed525788a
Sean McGrath <smcgrat@tcd.ie>
2017-02-14 12:40:47 +0000

    additional comments

--- abaqus-mp_host_list-slurm-generation.sh
+++ abaqus-mp_host_list-slurm-generation.sh
@@ -1,5 +1,15 @@
-# get a slurm environment output such as lonsdale-n[001,002]
-# & transmogrify it into something that mp_host_list compatible for abaqus
+# Running Abaqus in parallel on the clusters: http://www.tchpc.tcd.ie/node/1261
+# In order to run Abaqus in parallel it needs to know the compute nodes it will be running
+# It does not seem to be able to integrate with the slurm queueing system natively. 
+# This necessitates having a way to inform Abaqus of the compute nodes that slurm has made available to it.
+# The Abaqus `mp_host_list` environmental variable allows that
+# for further details please see the Abaqus documentation; https://www.simulia.com/support/v67/books/sgb67EF/default.htm?startat=ch04s01.html 
+# This code snippet translates the Slurm node list variable into a format that Abaqus can hopefully use 
+# by taking the slurm environment output such as lonsdale-n[001,002]
+# & transmogrifying it into something that is `mp_host_list` compatible for abaqus
+# Please note that this translation is naive, not very well designed and has barely been tested.
+# Ensure to include the snippet in your slurm batch submission script before the part of the script that runs Abaqus
+# Dependency: Python Hostlist - https://pypi.python.org/pypi/python-hostlist
 
 list="$SLURM_JOB_NODELIST"
 corecount=8 # specify the number of cores to be used by abaqus on each node
